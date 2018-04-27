41751a68ff95df3f0695385e781b2beef52d1e1e
Marcelo Salvador <marcelosalvador@MSalvador.local>
2018-04-27 07:08:17 -0300

    Initial Commit

---
+++ imc-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imc-app
+//
+//  Created by Marcelo Salvador on 27/04/18.
+//  Copyright © 2018 Marcelo Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
